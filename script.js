4209e97cff8283e547715a24ff97088ce3035cc9
E M P J De Saram <jayodya7desaram@gmail.com>
2023-12-22 00:13:12 +0530

    Designing the web page

--- script.js
+++ script.js
@@ -3,27 +3,45 @@ const todoList = [{
     dueDate:''
 }];
 
+
 function displayTodoList(){
     
     let todoListHTML = '';
 
-    for(let i=1; i<todoList.length; i++){
-        let todo = todoList[i];
+    for(let index=0; index<todoList.length; index++){
+        let todo = todoList[index];
         let {name, dueDate} = todo;
         let displayText = `
-            <p>
-                ${name} ${dueDate}
-                <button class="deleteButton>Delete</button>
-            </p>
+            <div class="text-div">
+                <div class="name-div">${name}</div>
+                <div class="dueDate-div">${dueDate}</div>
+                <div class="delete-button-div"><button class="deleteButton"><img src="images/delete icon.png" class="delete-icon"></button></div>
+            </div>
         `
 
         todoListHTML += displayText;
+
+        
     }
 
     document.querySelector('.todoList-container').innerHTML = todoListHTML;
+
+    document.querySelectorAll('.deleteButton').forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+            displayTodoList();
+        } );
+        
+    });
 }
 
 function addTodoList(){
+
+    todoList = JSON.parse(localStorage.getItem('todoList')) || [{
+        name:'',
+        dueDate:''
+    }];
+
     const inputElement = document.querySelector('.add-input');
     const name = inputElement.value;
 
@@ -34,6 +52,8 @@ function addTodoList(){
         name,
         dueDate
     })
+
+    localStorage.setItem('todoList', JSON.stringify(todoList));
 
     inputElement.value = '';
     inputDate.value = '';
